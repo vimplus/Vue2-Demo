353ec6b473c2a97dd64a1c60a0f184ae309ff635
JtxBoy <tx@vimplus.com>
2017-03-09 14:02:01 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const Vue = require('vue');
+const VueRouter = require('vue-router');
+const App = require('./app.vue');
+
+Vue.use(VueRouter);
+
+const router = new VueRouter(require('./router'));
+
+new Vue({
+    el: '#VueApp',
+    router: router,
+    render: h => h(App)
+})
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname + '/dist'),
+		publicPath: '',
+		filename: 'js/index.js'
+	},
+	module: {
+		rules: [
+			{
+				test: '/\.vue$/',
+				use: ['vue-loader']
+			}, {
+				test: '/\.js$/',
+				use: 'babel-loader',
+				include: [path.resolve(__dirname, 'src')]
+			}
+		]
+	}
+}
