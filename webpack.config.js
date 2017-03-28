67aff7b9c09243aa797d28c6ef3f8bd7a36c7ded
JtxBoy <tx@vimplus.com>
2017-03-28 19:38:05 +0800

    add sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
 	entry: './src/index.js',
 	output: {
@@ -16,8 +19,54 @@ module.exports = {
             },
 			{
 				test: /\.vue$/,
-				use: ['vue-loader']
+				loader: "vue-loader",
+				options: {
+		            loaders: {
+		                css: ExtractTextPlugin.extract({
+		                    use: 'css-loader'
+		                }),
+		                sass: ExtractTextPlugin.extract({
+		                    use: ["css-loader", "sass-loader"]
+		                })
+		            }
+		        }
+			},
+			{
+			    test: /\.(css|scss)$/,
+			    use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ["vue-style-loader", "css-loader", "postcss-loader", "sass-loader"]
+				})
+			},
+			{
+			    test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+			    use: [{
+			        loader: "url-loader",
+			        options: {
+			            limit: 10000,
+			            name: 'images/[name].[hash:6].[ext]'    // 将图片都放入images文件夹下，[hash:7]防缓存
+			        }
+			    }]
+			},
+			{
+			    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+			    use: [{
+			        loader: "url-loader",
+			        options: {
+			            limit: 10000,
+			            name: 'fonts/[name].[hash:6].[ext]'    // 将字体放入fonts文件夹下
+			        }
+			    }]
 			}
 		]
-	}
+	},
+	plugins: [
+		new ExtractTextPlugin({
+            filename: 'css/[name].[contenthash:8].css'
+        }),
+		new HtmlWebpackPlugin({
+		    filename: 'index.html',
+		    template: './index.html'
+		})
+	]
 }
